f90a318e92938c12b481f8dfa189ea4e2154eab7
Ali Farooq <m.aliafaroq@gmail.com>
2026-07-29 20:59:40 +0000

    Refactored to use class and changed storage
    I refactored the book constructor to use class and changed the storage to make it not save on page leave (since it caused issues) but on form submission for new book/card and status change. Also added a clear local memory button for people who want to start over from scratch.

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
-let myLibrary = []
 const libraryContainer = document.getElementById("library-container")
 const newBookForm = document.getElementById("form-popup")
 const newBook = document.getElementById("new-book")
 const body = document.querySelector("body")
 const header = document.querySelector("header")
+let myLibrary = []
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+        this.id = crypto.randomUUID();
+        this.info = function info() {
+            if (this.read === "read") {
+                console.log(`${this.title} by ${this.author}, ${this.pages} pages, read.`)
+            }
+            else if (this.read === "unread") {
+                console.log(`${this.title} by ${this.author}, ${this.pages} pages, not read yet.`)
+            }
+            else {throw Error("The read parameter must be written either as 'read' or 'unread'")}
+        }        
+    }
+
+}
+//Template Book Creation
 const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", "295", "unread")
 addBookToLibrary(theHobbit)
 updateBookDisplay(myLibrary)
-function Book(title, author, pages, read) {
-    if (!new.target) {
-        throw Error("You must use the 'new' operator to call the constructor")
-    }
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.id = crypto.randomUUID();
-    this.info = function info() {
-        if (this.read === "read") {
-            console.log(`${this.title} by ${this.author}, ${this.pages} pages, read.`)
-        }
-        else if (this.read === "unread") {
-            console.log(`${this.title} by ${this.author}, ${this.pages} pages, not read yet.`)
-        }
-        else {throw Error("The read parameter must be written either as 'read' or 'unread'")}
-    }
-}
 
 function addBookToLibrary(object) {
     myLibrary.push(object)
@@ -75,10 +76,12 @@ function updateBookDisplay(list) {
             if (book.read == "read") {
                 book.read = "unread"
                 bookStatus.textContent = `Status: ${book.read}`
+                localStorage.setItem("savedList", JSON.stringify(myLibrary));
             }
             else if (book.read == "unread") {
                 book.read = "read"
                 bookStatus.textContent = `Status: ${book.read}`
+                localStorage.setItem("savedList", JSON.stringify(myLibrary));
             }
         })
     })
@@ -88,7 +91,7 @@ newBook.addEventListener("click", () => {
     const myForm = document.createElement("form")
     myForm.setAttribute("id", "form-popup")
     myForm.setAttribute("method", "POST")
-
+    //Sets input for Book Title
     const labelBookTitle = document.createElement("label")
     labelBookTitle.textContent = "Book Title: "
     labelBookTitle.classList.add("label-font")
@@ -98,7 +101,7 @@ newBook.addEventListener("click", () => {
     inputBookTitle.setAttribute("id", "book-title")
     inputBookTitle.setAttribute("required", "")
     inputBookTitle.setAttribute("maxlength", "255")
-
+    //Sets input for Book Author
     const labelBookAuthor = document.createElement("label")
     labelBookAuthor.textContent = "Book Author: "
     labelBookAuthor.classList.add("label-font")
@@ -108,7 +111,7 @@ newBook.addEventListener("click", () => {
     inputBookAuthor.setAttribute("id", "book-author")
     inputBookAuthor.setAttribute("required", "")
     inputBookAuthor.setAttribute("maxlength", "50")
-
+    //Sets input for Book Pages
     const labelBookPages = document.createElement("label")
     labelBookPages.textContent = "Pages: "
     labelBookPages.classList.add("label-font")
@@ -118,14 +121,14 @@ newBook.addEventListener("click", () => {
     inputBookPages.setAttribute("id", "book-pages")
     inputBookPages.setAttribute("required", "")
     inputBookPages.setAttribute("maxlength", "10")
-
+    //Sets Radio Flex Containers for status Read or Unread
     const inputRadioContainer = document.createElement("div")
     inputRadioContainer.setAttribute("id", "input-radio-container")
     const unreadContainer = document.createElement("div")
     unreadContainer.setAttribute("id", "radio-align")
     const readContainer = document.createElement("div")
     readContainer.setAttribute("id", "radio-align")
-
+    //Sets radio buttons for Read and Unread
     const labelChooseUnread = document.createElement("label")
     labelChooseUnread.textContent = "Unread"
     labelChooseUnread.classList.add("label-font")
@@ -136,8 +139,6 @@ newBook.addEventListener("click", () => {
     inputChooseUnread.setAttribute("name", "read-or-not")
     inputChooseUnread.setAttribute("value", "unread")
     inputChooseUnread.setAttribute("required", "")
-
-
     const labelChooseRead = document.createElement("label")
     labelChooseRead.textContent = "Read"
     labelChooseRead.classList.add("label-font")
@@ -148,11 +149,11 @@ newBook.addEventListener("click", () => {
     inputChooseRead.setAttribute("name", "read-or-not")
     inputChooseRead.setAttribute("value", "read")
     inputChooseRead.setAttribute("required", "")
-
+    //Sets form submission button
     const submitButton = document.createElement("button")
     submitButton.setAttribute("type", "submit")
     submitButton.textContent = "submit"
-
+    //Sets form flex container
     const formContainer = document.createElement("div")
     formContainer.setAttribute("id", "form-container")
     body.insertBefore(formContainer, header);
@@ -181,15 +182,21 @@ newBook.addEventListener("click", () => {
         console.log(bookItem)
         addBookToLibrary(bookItem)
         updateBookDisplay(myLibrary)
+        localStorage.setItem("savedList", JSON.stringify(myLibrary));
     })
 })
-window.addEventListener("beforeunload", () => {
-    localStorage.setItem("savedList", JSON.stringify(myLibrary));
-});
 window.addEventListener("load", () => {
     let savedList = localStorage.getItem("savedList")
     if (savedList) {
         myLibrary = JSON.parse(savedList);
         updateBookDisplay(myLibrary)
     }
+});
+let clearLocalAndRefresh = document.createElement("button")
+body.appendChild(clearLocalAndRefresh)
+clearLocalAndRefresh.textContent = "Clear Local Memory"
+clearLocalAndRefresh.setAttribute("id", "clear-local-and-refresh")
+clearLocalAndRefresh.addEventListener("click", () => {
+    localStorage.clear();
+    location.reload();
 });
